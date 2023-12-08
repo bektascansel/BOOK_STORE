e7d374c86ce800be288f50eb7f4e0db65652c482
bektascansel <cansel.bektas6447@gmail.com>
2023-12-08 13:49:53 +0300

    Custom Exception Middleware

---
+++ Book Store(Custom Exception Middleware)/Book Store/BookOperations/CreateBooks/CreateBooksCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using Book_Store.BookOperations.CreatBooks;
+using FluentValidation;
+
+namespace Book_Store.BookOperations.CreateBooks
+{
+    public class CreateBooksCommandValidator : AbstractValidator<CreateBooksCommand>
+    {
+
+        public CreateBooksCommandValidator() { 
+        
+        
+            RuleFor(command=>command.model.GenreId).NotEmpty().GreaterThan(0);
+            RuleFor(command=>command.model.PageCount).NotEmpty().GreaterThan(0);
+            RuleFor(command=>command.model.PublishedDate.Date).NotEmpty().LessThan(DateTime.Now.Date);
+            RuleFor(command=>command.model.Title).NotEmpty().MinimumLength(3);
+
+        }
+
+    }
+}
